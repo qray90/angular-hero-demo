573b4a6188ebcdd0a6161fe6eb521ee2a68efbc7
ray <qray@foxmail.com>
2020-02-11 19:42:41 +0800

    1. data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BindingComponent } from './syntaxLearning/binding/binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BindingComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/syntaxLearning/binding/binding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.scss']
+})
+export class BindingComponent implements OnInit {
+  html = '<div>bound by [innerHTML] </div>';
+  html2 = '<div>bound by {{ }} </div>';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
